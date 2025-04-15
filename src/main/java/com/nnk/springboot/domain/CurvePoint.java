53f421d65028be92bd78ce197ab6a24f3b6b5deb
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-04-15 17:55:16 +0200

    Initial commit

---
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -0,0 +1,8 @@
+package com.nnk.springboot.domain;
+
+
+//@Entity
+//@Table(name = "curvepoint")
+public class CurvePoint {
+    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+}

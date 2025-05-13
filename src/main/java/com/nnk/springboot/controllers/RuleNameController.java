f917756dbe619577786d93edb04f9d0dddba88ac
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-05-13 13:54:25 +0200

    Add and Refacto RuleName Functionality

--- src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.exceptions.AlreadyExistsException;
+import com.nnk.springboot.exceptions.NotFoundException;
+import com.nnk.springboot.service.serviceImpl.RuleNameServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,47 +12,88 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
+@RequestMapping("/ruleName")
 public class RuleNameController {
-    // TODO: Inject RuleName service
 
-    @RequestMapping("/ruleName/list")
-    public String home(Model model)
-    {
-        // TODO: find all RuleName, add to model
+    private final RuleNameServiceImpl ruleNameService;
+
+    public RuleNameController(RuleNameServiceImpl ruleNameService) {
+        this.ruleNameService = ruleNameService;
+    }
+
+    @GetMapping("/list")
+    public String home(Model model) {
+        model.addAttribute("ruleNames", ruleNameService.findAll());
         return "ruleName/list";
     }
 
-    @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    @GetMapping("/add")
+    public String addRuleForm(RuleName ruleName, Model model) {
+        model.addAttribute("ruleName", ruleName);
         return "ruleName/add";
     }
 
-    @PostMapping("/ruleName/validate")
-    public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
-    }
+    @PostMapping("/validate")
+    public String validate(@Valid RuleName ruleName, BindingResult result, RedirectAttributes redirectAttributes) {
 
-    @GetMapping("/ruleName/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
-        return "ruleName/update";
-    }
+        if (result.hasErrors()) {return "ruleName/add";}
 
-    @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+        try {
+            ruleNameService.save(ruleName);
+            redirectAttributes.addFlashAttribute("success", "RuleName added successfully");
+        } catch (AlreadyExistsException e) {
+            result.rejectValue("name", "exists", e.getMessage() );
+            return "ruleName/add";
+        }
         return "redirect:/ruleName/list";
     }
 
-    @GetMapping("/ruleName/delete/{id}")
-    public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+
+        try {
+            RuleName ruleName = ruleNameService.findById(id);
+            model.addAttribute("ruleName", ruleName);
+            return "ruleName/update";
+        } catch (NotFoundException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage() + id);
+        }
+        return "redirect:/ruleName/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
+                                 BindingResult result, RedirectAttributes redirectAttributes) {
+
+        if (result.hasErrors()) {return "ruleName/update";}
+
+        try {
+            ruleName.setId(id);
+            ruleNameService.update(ruleName);
+            redirectAttributes.addFlashAttribute("success", "RuleName successfully updated");
+        } catch (AlreadyExistsException e) {
+            result.rejectValue("name", "exists", e.getMessage() );
+            return "ruleName/update";
+        } catch (NotFoundException e) {
+           result.rejectValue("error", e.getMessage() + id);
+        }
+
+        return "redirect:/ruleName/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteRuleName(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+
+        try {
+            ruleNameService.deleteById(id);
+            redirectAttributes.addFlashAttribute("success", "RuleName successfully deleted");
+        } catch (NotFoundException e) {
+            model.addAttribute("error", e.getMessage() + id);
+        }
         return "redirect:/ruleName/list";
     }
 }
---
+++ src/main/java/com/nnk/springboot/service/serviceImpl/RuleNameServiceImpl.java
@@ -0,0 +1,91 @@
+package com.nnk.springboot.service.serviceImpl;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.exceptions.CustomDataAccessException;
+import com.nnk.springboot.exceptions.NotFoundException;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class RuleNameServiceImpl {
+
+    private final Logger LOGGER = Logger.getLogger(RuleNameServiceImpl.class.getName());
+
+    private final RuleNameRepository ruleNameRepository;
+
+    public RuleNameServiceImpl(RuleNameRepository ruleNameRepository) {
+        this.ruleNameRepository = ruleNameRepository;
+    }
+
+    public List<RuleName> findAll() {
+       try {
+           return ruleNameRepository.findAll();
+       } catch (Exception e) {
+           LOGGER.warning("Error retrieving RuleName(s) from the database");
+           throw new CustomDataAccessException("Error retrieving RuleName(s) from the database", e);
+       }
+    }
+
+    public RuleName findById(Integer id) {
+        LOGGER.info("Retrieving RuleName with id: " + id);
+        return ruleNameRepository.findById(id).orElseThrow(() -> new NotFoundException("RuleName not found", id));
+    }
+
+    public void save(RuleName ruleName) {
+
+        try {
+            ruleNameRepository.save(ruleName);
+            LOGGER.info("RuleName saved successfully: " + ruleName);
+        } catch (Exception e) {
+            throw new CustomDataAccessException("Error saving RuleName to the database", e);
+        }
+    }
+
+    public void update(RuleName ruleName) {
+
+        if (!ruleNameRepository.existsById(ruleName.getId())) {
+            throw new NotFoundException("RuleName", ruleName.getId());
+        }
+
+        RuleName existingRuleName = ruleNameRepository.findById(ruleName.getId()).orElseThrow(()
+                -> new NotFoundException("RuleName", ruleName.getId()));
+
+        existingRuleName.setName(ruleName.getName());
+        existingRuleName.setDescription(ruleName.getDescription());
+        existingRuleName.setSqlStr(ruleName.getSqlStr());
+        existingRuleName.setSqlPart(ruleName.getSqlPart());
+        existingRuleName.setTemplate(ruleName.getTemplate());
+
+        ruleNameRepository.save(existingRuleName);
+    }
+
+    public void deleteById(Integer id) {
+
+        if (!ruleNameRepository.existsById(id)) {
+            throw new NotFoundException("RuleName", id);
+        }
+        ruleNameRepository.deleteById(id);
+        LOGGER.info("RuleName deleted successfully" + id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

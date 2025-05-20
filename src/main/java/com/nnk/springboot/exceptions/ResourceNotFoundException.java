d03eda431c2d87644fa7ffbeb37b39b339a5b57e
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-05-20 16:55:04 +0200

    revome InvalidPasswordException.java, ResourceNotFoundException.java, and update ThymeleafExceptionHandler.java

--- src/main/java/com/nnk/springboot/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.nnk.springboot.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/nnk/springboot/exceptions/ThymeleafExceptionHandler.java
+++ src/main/java/com/nnk/springboot/exceptions/ThymeleafExceptionHandler.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
